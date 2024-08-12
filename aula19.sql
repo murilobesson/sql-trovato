6967a5be3c7df74073296e4d65a6b56de82b1c5f
Murilo Besson <murilo.besson@outlook.com>
2024-08-12 17:28:29 -0300

    Adicionando metodos faltantes da aula

--- aula19.sql
+++ aula19.sql
@@ -24,3 +24,38 @@ set @varTexto3 = 'Criação de frase para teste de substring';
 
 --SUBSTRING
 select substring(@varTexto3, 1, 12);
+select patindex('&Trovato', @varTexto3)
+
+--REPLACE
+select replace('Murilo Besson', 'o', 'x')
+select replace(replace('Murilo Besson', 'o', 'x'), 'm', '?')
+
+select a.nome, a.data_nascimento, replace(a.sexo, 'M', 'Masculino') sexo
+from alunos a
+
+declare @cpf varchar(15)
+set @cpf = '111.222.333-44'
+
+select replace(replace(@cpf, '.', ''),'-', '')
+
+--REPLICATE
+select replicate('x', 20)
+
+--tamanho de coluna fixa em 50 posições
+--passo 1
+select len(nome) from alunos
+
+--passo 2
+select nome+replicate('x', 50 - len(nome))
+from alunos
+
+--passo 3
+select len(nome + replicate('x', 50 - len(nome))) as tamanho
+from alunos
+
+select len(nome + replicate('', 50 - len(nome)))
+from alunos
+
+--REVERSE
+select reverse('Murilo Besson')
+select reverse(nome) from alunos
